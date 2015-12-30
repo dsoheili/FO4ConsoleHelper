44dc30bc4018c5ac2693399c00a949c5ca8d86ba
dsoheili <dansoheili@gmail.com>
2015-12-30 15:58:29 -0800

    finish item script maker

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 
 int main () {
@@ -16,20 +17,26 @@ int main () {
   bool correctInput = false;
   string option1 = "item";
   string option2 = "perk";
+  int option = 0;
+  int itemAmount;
 
 // Loop until a correct input is given
   while (correctInput == false){
     cin >> scriptType;
 
     // Turn the input into lowercase to make string comparisons
-    // code snippet taken from: http://stackoverflow.com/a/313990
+    // Code snippet taken from: http://stackoverflow.com/a/313990
     transform(scriptType.begin(), scriptType.end(), scriptType.begin(), ::tolower);
     if (scriptType.compare(option1) == 0){
-      // item script selected
+      // Item script selected
+      option = 1;
+      cout << "Item script selected." << endl;
       correctInput = true;
     }
     else if (scriptType.compare(option2) == 0){
-      // perk script selected
+      // Perk script selected
+      option = 2;
+      cout << "Perk script selected." << endl;
       correctInput = true;
     }
     else{
@@ -38,6 +45,30 @@ int main () {
     }
   }
 
+// Item script creation
+if (option == 1){
+  cout << "Please enter the IDs of the items you want  and separate them by a comma (example a,b,c,d): ";
+  string itemList;
+  cin >> itemList;
+
+  // Amount of items added config
+  itemAmount = 1;
+
+  // Create and open the file to write to
+  ofstream myfile;
+  myfile.open ("items.txt");
+
+  // Code snippet written with the help of: http://stackoverflow.com/a/11719617
+  istringstream ss(itemList);
+  string token;
+
+  while(getline(ss, token, ',')) {
+    myfile << "player.additem " << token << " " << itemAmount << "\n";
+  }
+  // Close the file since modifications are complete
+  myfile.close();
+
+}
 
   return 0;
 }
