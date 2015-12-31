301ec226677400facea6ead81c320e3ca9b2541f
dsoheili <dansoheili@gmail.com>
2015-12-30 17:02:51 -0800

    complete perk addition script maker

--- main.cpp
+++ main.cpp
@@ -30,13 +30,13 @@ int main () {
     if (scriptType.compare(option1) == 0){
       // Item script selected
       option = 1;
-      cout << "Item script selected." << endl;
+      cout << "Item script selected.\n" << endl;
       correctInput = true;
     }
     else if (scriptType.compare(option2) == 0){
       // Perk script selected
       option = 2;
-      cout << "Perk script selected." << endl;
+      cout << "Perk script selected.\n" << endl;
       correctInput = true;
     }
     else{
@@ -47,7 +47,7 @@ int main () {
 
 // Item script creation
 if (option == 1){
-  cout << "Please enter the IDs of the items you want  and separate them by a comma (example a,b,c,d): ";
+  cout << "Please enter the IDs of the items you want and separate them by a comma (example a,b,c,d): ";
   string itemList;
   cin >> itemList;
 
@@ -58,12 +58,13 @@ if (option == 1){
   ofstream myfile;
   myfile.open ("items.txt");
 
-  // Code snippet written with the help of: http://stackoverflow.com/a/11719617
-  istringstream ss(itemList);
-  string token;
-
   // Add comment to file
   myfile << "! Created with FO4ConsoleHelper\n";
+
+  // Code snippet written with the help of: http://stackoverflow.com/a/11719617
+  // Break down the list and enter its contents in the file
+  istringstream ss(itemList);
+  string token;
 
   while(getline(ss, token, ',')) {
     myfile << "player.additem " << token << " " << itemAmount << "\n";
@@ -72,18 +73,48 @@ if (option == 1){
   myfile.close();
 
   // Notify user
-  cout << "\nScript created!" << endl;
+  cout << "\nScript created!\n" << endl;
   cout << "Move items.txt to Steam\\steamapps\\common\\Fallout 4" << endl;
   cout << "To run the script, open Fallout 4 and while playing press ~ on your keyboard." << endl;
   cout << "Then type: bat \"items.txt\" " << endl;
-  cout << "Make sure your item IDs are correct if there are any problems." << endl;
 
   }
 
   // Perk script creation
-  if (option == 2){
+  else if (option == 2){
+    cout << "Note: In order to obtain rank 4/4 in a perk, the previous ranks' IDs need to be added as well." << endl;
+    cout << "Please enter the IDs of the perks you want and separate them by a comma (example a,b,c,d): ";
+    string perkList;
+    cin >> perkList;
 
+    // Create and open the file to write to
+    ofstream myfile;
+    myfile.open ("perks.txt");
+
+    // Add comment to file
+    myfile << "! Created with FO4ConsoleHelper\n";
+
+    // Break down the list and enter its contents in the file
+    istringstream ss(perkList);
+    string token;
+
+    while(getline(ss, token, ',')) {
+      myfile << "player.addperk " << token << "\n";
+    }
+    // Close the file since modifications are complete
+    myfile.close();
+
+    cout << "\nScript created!\n" << endl;
+    cout << "Move perks.txt to Steam\\steamapps\\common\\Fallout 4" << endl;
+    cout << "To run the script, open Fallout 4 and while playing press ~ on your keyboard." << endl;
+    cout << "Then type: bat \"perks.txt\" " << endl;
   }
+
+  if ((option == 1) || (option == 2)){
+    cout << "Close the command console by pressing ~ again." << endl;
+    cout << "Make sure your item IDs are correct if there are any problems." << endl;
+  }
+
 
   return 0;
 }
